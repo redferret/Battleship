4b4f1dfd05c2e8542361b4627776d169956f3961
Richard <rdesilvey@gmail.com>
2021-03-30 18:25:41 -0600

    Add 'hit' test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -7,4 +7,16 @@ describe Ship do
     test_ship = Ship.new('cruiser', 3)
     expect(test_ship).to be_instance_of Ship
   end
+
+  describe '#hit' do
+    it 'reduces the health of the ship by 1' do
+      test_ship = Ship.new('cruiser', 3)
+
+      expect(test_ship.health).to eq 3
+
+      test_ship.hit
+
+      expect(test_ship.health).to eq 2
+    end
+  end
 end
