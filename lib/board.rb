2d0176a4e148443e93e2627e47fb913d2bed7038
MarkY <markcyen@yahoo.com>
2021-04-02 11:40:25 -0600

    Remove duplicate code

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 require './lib/ship'
 require './lib/cell'
 require './lib/coordinates'
+
 class Board
   attr_reader :cells
 
@@ -20,33 +21,13 @@ class Board
 
   def valid_placement?(ship, coords)
     ship.length == coords.length &&
-      consecutive?(coords)
+      Coordinates.consecutive?(coords)
   end
 
-  def consecutive?(coords)
+  def place(ship, coords)
+  end
 
-    y_coords = coords.map do |coord|
-      coord[0].ord
-    end
-    check_y_hor = y_coords.each_cons(2).all? do |a, b|
-      b == a
-    end
-
-    check_y_ver = y_coords.each_cons(2).all? do |a, b|
-      b == a + 1
-    end
-
-    x_coords = coords.map do |coord|
-      coord[-1].to_i
-    end
-    check_x_hor = x_coords.each_cons(2).all? do |a, b|
-      b == a + 1
-    end
-    check_x_ver = x_coords.each_cons(2).all? do |a, b|
-      b == a
-    end
-    (check_y_hor && check_x_hor) || (check_y_ver && check_x_ver)
-
+  def render()
   end
 
 end
