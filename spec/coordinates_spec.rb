ec2d17d914a7903e6aa7d3bdf536d5ab935d541c
Richard <rdesilvey@gmail.com>
2021-04-02 11:25:57 -0600

    Refactor Coordinates

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -2,88 +2,80 @@ require 'rspec'
 require './lib/coordinates.rb'
 
 describe Coordinates do
-  context '#initialize' do
-    it 'exists' do
-      coords = Coordinates.new("A1 A2 A3")
-      expect(coords).to be_instance_of Coordinates
-    end
-
-    it 'has coordinates' do
-      coords = Coordinates.new("A1 A2 A3")
-
-      expected_coords = ["A1", "A2", "A3"]
-      expect(coords.to_a).to eq expected_coords
+  context '#diagonal?' do
+    it 'tests if the coordinates are diagonal' do
+      coords = Coordinates::to_a("A1 B2 C3")
+      expect(Coordinates::diagonal?(coords)).to eq true
     end
   end
 
-  context '#are_diagonal?' do
-    it 'tests if the coordinates are diagonal' do
-      coords = Coordinates.new("A1 B2 C3")
-      expect(coords.are_diagonal?).to eq true
-    end
-
+  context '#not_diagonal' do
     it 'tests if the coordinates are not diagonal' do
-      coords = Coordinates.new("A1 B1 C1")
-      expect(coords.are_diagonal?).to eq false
+      coords = Coordinates::to_a("A1 B1 C1")
+      expect(Coordinates::diagonal?(coords)).to eq false
 
-      coords = Coordinates.new("A1 A2 A3")
-      expect(coords.are_diagonal?).to eq false
+      coords = Coordinates::to_a("A1 A2 A3")
+      expect(Coordinates::diagonal?(coords)).to eq false
     end
   end
 
   context '#vertical?' do
     it 'tests if the coordinates are vertical' do
-      coords = Coordinates.new("A1 B1 C1")
-      expect(coords.are_vertical?).to eq true
+      coords = Coordinates::to_a("A1 B1 C1")
+      expect(Coordinates::vertical?(coords)).to eq true
     end
+  end
 
+  context '#not_vertical' do
     it 'tests if the coordinates are not vertical' do
-      coords = Coordinates.new("A1 A2 A3")
-      expect(coords.are_vertical?).to eq false
+      coords = Coordinates::to_a("A1 A2 A3")
+      expect(Coordinates::vertical?(coords)).to eq false
     end
   end
 
   context '#horizontal' do
     it 'tests if the coordinates are horizontal' do
-      coords = Coordinates.new("A1 A2 A3")
-      expect(coords.are_horizontal?).to eq true
+      coords = Coordinates::to_a("A1 A2 A3")
+      expect(Coordinates::horizontal?(coords)).to eq true
     end
+  end
 
+  context '#not_horizontal' do
     it 'tests if the coordinates are not horizontal' do
-      coords = Coordinates.new("A1 B1 C1")
-      expect(coords.are_horizontal?).to eq false
+      coords = Coordinates::to_a("A1 B1 C1")
+      expect(Coordinates::horizontal?(coords)).to eq false
     end
   end
 
   context '#intersects?' do
     it 'tests if coordinates are intersecting' do
-      coords_1 = Coordinates.new("A1 A2 A3")
-      coords_2 = Coordinates.new("A2 B2 C2")
+      coords_1 = Coordinates::to_a("A1 A2 A3")
+      coords_2 = Coordinates::to_a("A2 B2 C2")
 
-      intersection = coords_1.intersects?(coords_2)
+      intersection = Coordinates::intersects?(coords_1, coords_2)
 
       expect(intersection).to eq true
     end
 
     it 'tests if coordinates are not intersecting' do
-      coords_1 = Coordinates.new("A1 A2 A3")
-      coords_2 = Coordinates.new("B2 C2 D2")
+      coords_1 = Coordinates::to_a("A1 A2 A3")
+      coords_2 = Coordinates::to_a("B2 C2 D2")
 
-      intersection = coords_1.intersects?(coords_2)
+      intersection = Coordinates::intersects?(coords_1, coords_2)
 
       expect(intersection).to eq false
     end
   end
 
-  context '#are_consecutive' do
+  context '#consecutive' do
     it 'tests that the coordinates are consecutive' do
-      coords = Coordinates.new("A1 A2 A3")
-      expect(coords.are_consecutive?).to eq true
+      coords = Coordinates::to_a("A1 A2 A3")
+      expect(Coordinates::consecutive?(coords)).to eq true
     end
 
     it 'tests that the coordinates are not consecutive' do
-      coords = Coordinates.new("A1 A3")
-      expect(coords.are_consecutive?).to eq false
+      coords = Coordinates::to_a("A1 A3")
+      expect(Coordinates::consecutive?(coords)).to eq false
     end
   end
 end
