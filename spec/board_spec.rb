729c0a56a8b13d390999beac29624e01c75ffd02
MarkY <markcyen@yahoo.com>
2021-04-02 09:01:47 -0600

    Test for board cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,16 @@ describe Board do
       board = Board.new
       expect(board).to be_instance_of(Board)
     end
-end
+
+    it 'check board cells' do
+      board = Board.new
+
+      expected_cells = ["A1", "A2", "A3", "A4",
+                        "B1", "B2", "B3", "B4",
+                        "C1", "C2", "C3", "C4",
+                        "D1", "D2", "D3", "D4",]
+
+      expect(board.cells.keys).to eq(expected_cells)
+    end
+
+  end
