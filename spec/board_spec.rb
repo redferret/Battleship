5eb74fbc6f5192e9189c9d4e1e3fe8daadfb6090
MarkY <markcyen@yahoo.com>
2021-04-02 09:04:15 -0600

    Create test for valid placement for length and consecutive coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,3 +24,40 @@ describe Board do
     end
 
   end
+
+  context '#valid_placement?' do
+    it 'check length of ship with same number of coordinates' do
+      board = Board.new
+      carrier = Ship.new("Carrier", 5)
+      coords_long = Coordinates.new("C1 C2 C3 C4").to_a
+
+      expect(board.valid_placement?(carrier, coords_long)).to eq false
+
+      cruiser = Ship.new("Cruiser", 3)
+      coords = Coordinates.new("A1 A2 A3").to_a
+
+      expect(board.valid_placement?(cruiser, coords)).to eq true
+    end
+
+    it 'checks coordinates of ship are consecutive' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser 1", 3)
+      coords_1 = Coordinates.new("A1 A2 A3").to_a
+
+      expect(board.valid_placement?(cruiser, coords_1)).to eq true
+
+      submarine = Ship.new("Submarine", 3)
+      coords_2 = Coordinates.new("B1 C1 D1").to_a
+
+      expect(board.valid_placement?(submarine, coords_2)).to eq true
+
+      destroyer = Ship.new("Destoyer", 2)
+      coords_3 = Coordinates.new("A1 C1").to_a
+
+      expect(board.valid_placement?(submarine, coords_3)).to eq false
+
+      cruiser = Ship.new("Cruiser 2", 2)
+      coords_4 = Coordinates.new("A1 A2 A4").to_a
+
+      expect(board.valid_placement?(submarine, coords_4)).to eq false
+    end
