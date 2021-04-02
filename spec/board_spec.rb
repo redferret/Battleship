42139a0346d4209a051216dcfe36c63efdb8a89f
MarkY <markcyen@yahoo.com>
2021-04-02 11:43:06 -0600

    Refactor test
    To match the refactoring of the coordinate class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,15 +26,16 @@ describe Board do
   end
 
   context '#valid_placement?' do
+
     it 'check length of ship with same number of coordinates' do
       board = Board.new
       carrier = Ship.new("Carrier", 5)
-      coords_long = Coordinates.new("C1 C2 C3 C4").to_a
+      coords_long = Coordinates.to_a("C1 C2 C3 C4")
 
       expect(board.valid_placement?(carrier, coords_long)).to eq false
 
       cruiser = Ship.new("Cruiser", 3)
-      coords = Coordinates.new("A1 A2 A3").to_a
+      coords = Coordinates.to_a("A1 A2 A3")
 
       expect(board.valid_placement?(cruiser, coords)).to eq true
     end
@@ -42,22 +43,22 @@ describe Board do
     it 'checks coordinates of ship are consecutive' do
       board = Board.new
       cruiser = Ship.new("Cruiser 1", 3)
-      coords_1 = Coordinates.new("A1 A2 A3").to_a
+      coords_1 = Coordinates.to_a("A1 A2 A3")
 
       expect(board.valid_placement?(cruiser, coords_1)).to eq true
 
       submarine = Ship.new("Submarine", 3)
-      coords_2 = Coordinates.new("B1 C1 D1").to_a
+      coords_2 = Coordinates.to_a("B1 C1 D1")
 
       expect(board.valid_placement?(submarine, coords_2)).to eq true
 
       destroyer = Ship.new("Destoyer", 2)
-      coords_3 = Coordinates.new("A1 C1").to_a
+      coords_3 = Coordinates.to_a("A1 C1")
 
       expect(board.valid_placement?(submarine, coords_3)).to eq false
 
       cruiser = Ship.new("Cruiser 2", 2)
-      coords_4 = Coordinates.new("A1 A2 A4").to_a
+      coords_4 = Coordinates.to_a("A1 A2 A4")
 
       expect(board.valid_placement?(submarine, coords_4)).to eq false
     end
@@ -65,21 +66,22 @@ describe Board do
     it 'make sure ship coordinates are not diagonal' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
-      coords_1 = Coordinates.new("A1 B2 C3").to_a
+      coords_1 = Coordinates.to_a("A1 B2 C3")
 
       expect(board.valid_placement?(cruiser, coords_1)).to eq false
 
       submarine = Ship.new("Submarine", 3)
-      coords_2 = Coordinates.new("A1 B1 C1").to_a
+      coords_2 = Coordinates.to_a("A1 B1 C1")
 
       expect(board.valid_placement?(cruiser, coords_2)).to eq true
 
     end
+
   context '#place' do
     it 'place ship at given coordinates' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
-      coordinates = Coordinates.new("A1 A2 A3")
+      coords = Coordinates.to_a("A1 A2 A3")
       board.place(cruiser, coordinates)
       cell_1 = board.cells["A1"]
       cell_2 = board.cells["A2"]
@@ -89,32 +91,31 @@ describe Board do
       expect(cell_1.ship).to eq(cell_3.ship)
       expect(cell_2.ship).to eq(cell_3.ship)
       expect(cell_1.ship).not_to eq(cell_4.ship)
-      expect(valid_placement?(cruiser, coordinates)).to eq true
+      expect(valid_placement?(cruiser, coords)).to eq true
     end
 
-    it 'check overlap of ship placement' do
+    it 'check overlap of ships placement' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
-      coordinates_1 = Coordinates.new("A1 A2 A3")
-      board.place(cruiser, coordinates_1)
+      coords_1 = Coordinates.to_a("A1 A2 A3")
+      board.place(cruiser, coords_1)
       cell_1 = board.cells["A1"]
       cell_2 = board.cells["A2"]
       cell_3 = board.cells["A3"]
 
       submarine = Ship.new("Submarine", 3)
-      coordinates_2 = Coordinates.new("A1 B1 C1")
-      board.place(submarine, coorinates_2)
+      coords_2 = Coordinates.to_a("A1 B1 C1")
+      board.place(submarine, coords_2)
       cell_4 = board.cells["A1"]
       cell_5 = board.cells["B1"]
       cell_6 = board.cells["C1"]
 
-      expect(valid_placement?(submarine, coorinates_2)).to eq false
+      expect(valid_placement?(submarine, coords_2)).to eq false
     end
   end
 
   context '#render' do
     it 'render default board' do
-      # render board by default
       board = Board.new
       board.render
       expected = "  1 2 3 4 \n
@@ -129,8 +130,8 @@ describe Board do
     it 'render human players board' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
-      coordinates_1 = Coordinates.new("A1 A2 A3")
-      board.place(cruiser, coordinates_1)
+      coords_1 = Coordinates.to_a("A1 A2 A3")
+      board.place(cruiser, coords_1)
       cell_1 = board.cells["A1"]
       cell_2 = board.cells["A2"]
       cell_3 = board.cells["A3"]
