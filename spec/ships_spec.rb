a079539a5a8e830c3a1e7571faf84e0ce937b256
Richard <rdesilvey@gmail.com>
2021-04-05 10:06:57 -0600

    Test get_ship

--- spec/ships_spec.rb
+++ spec/ships_spec.rb
@@ -14,5 +14,30 @@ describe Ships do
     end
   end
 
+  context '#get_ship' do
+    it 'returns a ship based on ids' do
+      ships = Ships.new
+
+      expected_names = [
+        'Carrier',
+        'Battleship',
+        'Destroyer',
+        'Submarine',
+        'Patrol Boat'
+      ]
+
+      expected_lengths = [5, 4, 3, 3, 2]
+
+      for id in (0..4) do
+        ship = ships.get_ship(id)
+        expected_length = expected_lengths[id]
+        expected_name = expected_names[id]
+
+        expect(ship).to be_instance_of Ship
+        expect(ship.length).to eq expected_length
+        expect(ship.name).to eq expected_name
+      end
+    end
+  end
 
 end
