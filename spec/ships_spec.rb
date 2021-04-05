0838938d1ae36281ec32b7219630587b94277c39
Richard <rdesilvey@gmail.com>
2021-04-05 09:49:25 -0600

    Test that Ships has a ships list

--- spec/ships_spec.rb
+++ spec/ships_spec.rb
@@ -7,5 +7,12 @@ describe Ships do
       ships = Ships.new
       expect(ships).to be_instance_of Ships
     end
+
+    it 'has ships list' do
+      playable_ships = Ships.new
+      expect(playable_ships.ships).not_to eq nil
+    end
   end
+
+
 end
