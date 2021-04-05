abdf7c7e175ef92b34bdddfd3eaf121fc8f681a5
Richard <rdesilvey@gmail.com>
2021-04-05 12:32:11 -0600

    Implement get_ship

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,9 +2,19 @@ require './lib/player'
 
 class ComputerPlayer < Player
   attr_reader :board
+
+  def initialize(board)
+    super
+    @computer_ships = Ships.new
+  end
+
   def get_random_coordinate
     y_range = rand(1..@board.size) + 64
     x_range = rand(1..@board.size)
 
     "#{y_range.chr}#{x_range}"
+  end
+
+  def get_ship(ship_id)
+    @computer_ships.ships[ship_id]
   end
