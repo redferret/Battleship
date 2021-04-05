79cfe88071d1f701e112b000d13ea8223d585bab
Richard <rdesilvey@gmail.com>
2021-04-05 11:44:48 -0600

    Test that parent class gets a board

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -10,5 +10,9 @@ describe ComputerPlayer do
 
         expect(computer_player).to be_instance_of ComputerPlayer
       end
-    end
+      it 'gives board to the parent class' do
+        board = double('board')
+        computer_player = ComputerPlayer.new(board)
+        expect(computer_player.board).to eq board
+      end
 end
