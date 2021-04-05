f27d548450121219f920855fb4d08081a0882cba
Richard <rdesilvey@gmail.com>
2021-04-05 12:07:54 -0600

    Test get_random_coordinate

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -10,9 +10,32 @@ describe ComputerPlayer do
 
         expect(computer_player).to be_instance_of ComputerPlayer
       end
+
       it 'gives board to the parent class' do
         board = double('board')
         computer_player = ComputerPlayer.new(board)
         expect(computer_player.board).to eq board
       end
+    end
+
+    context '#get_random_coordinate' do
+      it 'returns a valid random coordinate' do
+        test_board_cells = {
+          "A1": double('cell1'),
+          "A2": double('cell2'),
+          "B1": double('cell3'),
+          "B2": double('cell4')
+        }
+        possible_coordinates = ["A1", "A2", "B1", "B2"]
+        board = double('board')
+        allow(board).to receive(:cells).and_return(test_board_cells)
+
+        computer_player = ComputerPlayer.new(board)
+        random_coordinate = computer_player.get_random_coordinate
+
+        is_correct_coordinate = possible_coordinates.include?(random_coordinate)
+
+        expect(is_correct_coordinate).to eq true
+      end
+    end
 end
