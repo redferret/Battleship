7fd17ab3d2a704e685ab0c0eb381937d02349fd6
MarkY <markcyen@yahoo.com>
2021-04-06 20:38:43 -0600

    Fix issue with print_boards

--- battle_ship.rb
+++ battle_ship.rb
@@ -1,18 +1,18 @@
 require './lib/human_player'
 require './lib/computer_player'
 
-def print_boards
-  puts "=============COMPUTER BOARD============="
-  puts computer_player.render_board
-  puts "==============PLAYER BOARD=============="
-  puts human_player.render_board(true)
-end
-
 human_board = Board.new(10)
 computer_board = Board.new(10)
 
 human_player = HumanPlayer.new(human_board)
 computer_player = ComputerPlayer.new(computer_board)
+
+def print_boards(computer_player, human_player)
+  puts "=============COMPUTER BOARD============="
+  puts computer_player.render_board
+  puts "==============PLAYER BOARD=============="
+  puts human_player.render_board(true)
+end
 
 puts "Welcome to BATTLESHIP"
 print "Enter p to play. Enter q to quit: "
@@ -36,7 +36,7 @@ when 'p'
   human_player.place_ships
 
   loop do
-    print_boards
+    print_boards(computer_player, human_player)
 
     human_player.take_turn(computer_board)
     computer_player.take_turn(human_board)
@@ -51,8 +51,9 @@ when 'p'
       break
     end
   end
+
+  print_boards(computer_player, human_player)
+
 when 'q'
   puts 'Bye'
 end
-
-print_boards
